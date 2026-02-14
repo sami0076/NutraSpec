3809b59e3eae413a148e7a4de958cfe27dd11f14
Sami Saleh <samisaleh07@gmail.com>
2026-02-14 18:03:51 -0500

    attempted CORS fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -43,9 +43,11 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allow_headers=["*"],
+    expose_headers=["*"],
+    max_age=3600,
 )
 
 app.include_router(analyze.router, prefix="/analyze", tags=["analyze"])
